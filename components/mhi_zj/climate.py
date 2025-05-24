7d1248ef954fc1c50a9c1d4b3d5f77f63ad3a6a0
shammysha <65885873+shammysha@users.noreply.github.com>
2025-05-24 20:44:57 +0300

    Fix deprecated warnings (climate_ir_with_receiver_schema)

--- components/mhi_zj/climate.py
+++ components/mhi_zj/climate.py
@@ -30,7 +30,7 @@ VERTICAL_DIRECTIONS = {
 }
 
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema.extend(
     {
         cv.GenerateID(): cv.declare_id(MhiClimate),
         cv.Optional(CONF_HORIZONTAL_DEFAULT, default="middle"): cv.enum(
